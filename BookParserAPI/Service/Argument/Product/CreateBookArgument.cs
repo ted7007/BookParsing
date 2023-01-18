47232b7ee18b0fca71502ad4c549e3e64786de56
Александр Косилов <ted7007@yandex.ru>
2023-01-18 18:37:19 +1000

    fix: edit models from template

--- BookParserAPI/Service/Argument/Product/CreateBookArgument.cs
+++ BookParserAPI/Service/Argument/Product/CreateBookArgument.cs
@@ -4,7 +4,19 @@ public class CreateBookArgument
 {
     public string Name { get; set; }
 
-    public decimal Price { get; set; }
+    public string Author { get; set; }
 
     public string Description { get; set; }
+
+    public string Year { get; set; }
+    
+    //todo разобраться с картинкой
+
+    public string ISBN { get; set; }
+
+    public int PagesCount { get; set; }
+
+    public string Genre { get; set; }
+
+    public List<string> Tags { get; set; }
 }
--- BookParserAPI/Service/Argument/Product/UpdateBookArgument.cs
+++ BookParserAPI/Service/Argument/Product/UpdateBookArgument.cs
@@ -6,7 +6,19 @@ public class UpdateBookArgument
 
     public string Name { get; set; }
 
-    public decimal Price { get; set; }
+    public string Author { get; set; }
 
     public string Description { get; set; }
+
+    public string Year { get; set; }
+    
+    //todo разобраться с картинкой
+
+    public string ISBN { get; set; }
+
+    public int PagesCount { get; set; }
+
+    public string Genre { get; set; }
+
+    public List<string> Tags { get; set; }
 }
