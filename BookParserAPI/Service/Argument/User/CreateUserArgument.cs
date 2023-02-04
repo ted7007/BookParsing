9258c7b73522d133dab6385a1fe2552e26c1a019
Александр Косилов <ted7007@yandex.ru>
2023-02-04 19:38:08 +1000

    feat: add user service

---
+++ BookParserAPI/Service/Argument/User/CreateUserArgument.cs
@@ -0,0 +1,8 @@
+﻿namespace BookParserAPI.Service.Argument.User;
+
+public class CreateUserArgument
+{
+    public string Login { get; set; }
+
+    public string Password { get; set; }
+}
---
+++ BookParserAPI/Service/User/IUserService.cs
@@ -0,0 +1,10 @@
+﻿using BookParserAPI.Service.Argument.User;
+
+namespace BookParserAPI.Service.User;
+
+public interface IUserService
+{
+    public Task<Models.User> CreateAsync(CreateUserArgument argument);
+
+    public Task<Models.User> GetByLoginAsync(string login);
+}
