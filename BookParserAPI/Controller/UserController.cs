1ce2b4a88009f620c411c284bab99e41a17ac9e6
Александр Косилов <ted7007@yandex.ru>
2023-02-05 00:40:17 +1000

    feat: add LogIn to controller

--- BookParserAPI/Controller/UserController.cs
+++ BookParserAPI/Controller/UserController.cs
@@ -2,6 +2,7 @@
 using BookParserAPI.Dto.Input.User;
 using BookParserAPI.Dto.Output.User;
 using BookParserAPI.Models;
+using BookParserAPI.Security;
 using BookParserAPI.Service.Argument.User;
 using BookParserAPI.Service.User;
 using Microsoft.AspNetCore.Mvc;
@@ -13,20 +14,31 @@ namespace BookParserAPI.Controller;
 public class UserController
 {
     private readonly IUserService _userService;
+    private readonly IAuthenticationService _authService;
     private readonly IMapper _mapper;
 
-    public UserController(IUserService userService, IMapper mapper)
+    public UserController(IUserService userService, IAuthenticationService authService, IMapper mapper)
     {
         _userService = userService;
+        _authService = authService;
         _mapper = mapper;
     }
 
     [HttpPost]
-    public async Task<ActionResult<UserDto>> Register(RegisterDto dto)
+    public async Task<ActionResult<UserDto>> Register(RegisterModel model)
     {
-        var mappedArgument = _mapper.Map<RegisterDto, CreateUserArgument>(dto);
+        var mappedArgument = _mapper.Map<RegisterModel, CreateUserArgument>(model);
         var result = await _userService.CreateAsync(mappedArgument);
         var mappedResult = _mapper.Map<User, UserDto>(result);
         return new OkObjectResult(mappedResult);
+    }
+
+    [HttpPost("Login")]
+    public async Task<ActionResult<string>> LogIn(LoginModel model)
+    {
+        var token = await _authService.GetTokenAsync(model);
+        if (token is null)
+            return new UnauthorizedResult();
+        return new OkObjectResult(token);
     }
 }
